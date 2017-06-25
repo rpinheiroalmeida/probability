5f551d5a7c7d4857075736a4ba4f7258c3075058
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2017-06-24 21:33:34 -0300

    Initial commit

---
+++ probabilit_test.go
@@ -0,0 +1,90 @@
+package probability
+
+import (
+	"testing"
+)
+
+func TestUniformPdf(t *testing.T) {
+	cases := []struct {
+		x    float64
+		want int
+	}{
+		{0.65, 1},
+		{1, 0},
+		{-1, 0},
+		{-0.4, 0},
+	}
+
+	for _, c := range cases {
+		got := UniformPdf(c.x)
+		if got != c.want {
+			t.Errorf("UniformPdf(%f) want: %d but got: %d", c.x, c.want, got)
+		}
+	}
+}
+
+func TestUniformCdf(t *testing.T) {
+	cases := []struct {
+		x    float64
+		want float64
+	}{
+		{1.0, 1.0},
+		{0.3, 0.3},
+		{-0.2, 0.0},
+	}
+
+	for _, c := range cases {
+		got := UniformCdf(c.x)
+		if got != c.want {
+			t.Errorf("UniformCdf(%f) want: %f but got: %f", c.x, c.want, got)
+		}
+	}
+}
+
+func TestNormalPdf(t *testing.T) {
+	cases := []struct {
+		x, mu, sigma float64
+		want         float64
+	}{
+		{1.0, MU, SIGMA, 0.24197072451914337},
+		{2.0, MU, SIGMA, 0.05399096651318806},
+		{12.0, MU, SIGMA, 2.1463837356630605e-32},
+		{0.0, MU, SIGMA, 0.3989422804014327},
+		{-10.0, MU, SIGMA, 7.69459862670642e-23},
+		{-10.0, 0.0, SIGMA, 7.69459862670642e-23},
+		{-10.0, 2, 1, 2.1463837356630605e-32},
+		{-10.0, 2, 2, 3.037941424911643e-09},
+		{-1.0, 2, 1, 0.0044318484119380075},
+		{10.0, 2, 3, 0.0037986620079324806},
+	}
+	for _, c := range cases {
+		got := NormalPdf(c.x, c.mu, c.sigma)
+		if got != c.want {
+			t.Errorf("NormalPdf(%v, %v, %v) want: %v but got: %v",
+				c.x, c.sigma, c.mu, c.want, got)
+		}
+	}
+}
+
+func TestNormalCdf(t *testing.T) {
+	cases := []struct {
+		x, mu, sigma float64
+		want         float64
+	}{
+		{10.0, MU, SIGMA, 1},
+		{3.0, MU, SIGMA, 0.9986501019683699},
+		{1.0, MU, SIGMA, 0.8413447460685429},
+		{1.0, 1, 2, 0.5},
+		{1.0, 2, 2, 0.3085375387259869},
+		{1.0, -2, SIGMA, 0.9986501019683699},
+		{3.0, 2, -2, 0.3085375387259869},
+		{-5.0, -3, -2, 0.8413447460685429},
+	}
+	for _, c := range cases {
+		got := NormalCdf(c.x, c.mu, c.sigma)
+		if got != c.want {
+			t.Errorf("NormalCdf(%v, %v, %v) want: %v but got: %v",
+				c.x, c.mu, c.sigma, c.want, got)
+		}
+	}
+}
---
+++ probability.go
@@ -0,0 +1,37 @@
+package probability
+
+import "math"
+
+const SIGMA = 1.0
+const MU = 0.0
+
+func UniformPdf(x float64) (result int) {
+	result = 0
+	if x >= 0 && x < 1 {
+		result = 1
+	}
+	return
+}
+
+func UniformCdf(x float64) (result float64) {
+	result = 1
+	if x < 0 {
+		result = 0
+	} else if x < 1 {
+		result = x
+	}
+	return
+}
+
+func NormalPdf(x, mu, sigma float64) float64 {
+	sqrtTwoPi := math.Sqrt(2 * math.Pi)
+	powXmu := math.Pow((x - mu), 2)
+	exp := math.Exp(-(powXmu / (2 * math.Pow(sigma, 2))))
+	return exp / (sqrtTwoPi * sigma)
+}
+
+func NormalCdf(x, mu, sigma float64) float64 {
+	erf := math.Erf((x - mu) / math.Sqrt2 / sigma)
+	return (1 + erf) / float64(2)
+	// return (1 + math.erf((x - mu) / math.sqrt(2) / sigma) ) / 2
+}
